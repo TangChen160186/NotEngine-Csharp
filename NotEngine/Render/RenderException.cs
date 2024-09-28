b7d082bbe85153ef0b4cc6df056b2efe9a2d4d20
Chen Tang <1601865659@qq.com>
2024-09-28 13:42:52 +0800

    add third part

--- NotEngine/Render/RenderException.cs
+++ NotEngine/Render/RenderException.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace NotEngine.Render;
 
-namespace NotEngine.Render
+internal class RenderException
 {
-    internal class RenderException
-    {
-    }
 }
--- NotEngine/Render/Texture2D.cs
+++ NotEngine/Render/Texture2D.cs
@@ -2,5 +2,20 @@
 
 internal abstract class Texture2D: IDisposable
 {
+    public bool IsDisposed { get; protected set; }
+    protected virtual void ReleaseUnmanagedResources()
+    {
+        IsDisposed = true;
+    }
 
+    public void Dispose()
+    {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+
+    ~Texture2D()
+    {
+        ReleaseUnmanagedResources();
+    }
 }
